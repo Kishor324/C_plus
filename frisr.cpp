a504a93915846555aae8954de10a99f2b16cde46
kishor324 <kishorbhatt157@gmail.com>
2025-07-09 08:36:23 +0545

    first program

---
+++ frisr.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std ;
+int main () {
+     cout<<"HEllo World!"<<
+     endl;
+     return 0;
+     
+
+}
+
+// #include <stdio.h>
+// int main (){
+//      printf("hello wrold");
+//      return 0;
+// }
