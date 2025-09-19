d170b7a78f2a8df93ff76a2f3f500b1c8f145dd9
kishor324 <kishorbhatt157@gmail.com>
2025-09-19 17:20:58 +0545

    Variable scope & Data types

---
+++ video.4/tul4.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+ int glo =6;
+ void sum(){
+     int a;
+      cout<<glo;
+ }
+int main (){
+    int glo=9;
+    glo = 78;
+   
+//    int a =5; int b=6;
+int a =5 , b=6; 
+ float p1= 3.36;
+ char c = 'P';
+  bool is_true= true;
+  sum();
+cout <<glo<<is_true;
+ // cout<< "This is tutorial 4. Here \nthe valuue of a is "<<a<<".The value od b is "<<b;
+    //  cout << "\n the value od pie is "<<p1;
+    //  cout << "\n the value od pie is "<<c;
+    return 0;
+}
