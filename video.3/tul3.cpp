284f3391936bf319fe243c198dbe147f63d1397c
kishor324 <kishorbhatt157@gmail.com>
2025-09-19 17:16:48 +0545

    variables and comments in cpp

---
+++ video.3/tul3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+//  This program is creatde by kisohor
+/*
+ is the 
+  the
+   is 
+   */
+
+
+int main(){
+    int sum = 6;
+    // cout << "hello world"<< endl<< "hello kishor";
+    // cout<< endl;
+    // cout<<"\n"; 
+    cout << "hello world"<< sum;
+    return 0;
+}
