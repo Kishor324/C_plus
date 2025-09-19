56bab665a62d4ca4ec79818df7994366986bf520
kishor324 <kishorbhatt157@gmail.com>
2025-09-19 17:25:06 +0545

    References variables and Typecasting in cpp

---
+++ video.7/tul7.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+int c =45;
+ int main (){
+    // ********** types of data **********
+//     int a,b,c;
+//     cout <<" Enter the value of a:"<<endl;
+//      cin>>a;
+//     cout <<" Enter the value of b:"<<endl;
+//   cin>>b;
+// //     cout <<" Enter the value of c:"<<endl;
+// //  cin>>c;
+// c = a+b;
+//  cout<<"the sum is "<<c;
+//  cout<< "The golabal c is"<< c;
+
+// /******* float,double flaot and long double flaot ********/
+//  float d= 34.3f;
+//  long double e =334.3l;
+//  cout<< " the va;ue of d is "<<d<<endl<<"ther value of e is"<<e;
+
+// we use the  f  and l function for float and long flaot for references  
+
+
+/******* Reference variable ********/
+// Rohan das-------> monery--> jadu---->
+//  float x = 4555;
+//  float & y = x;
+//  cout<<x<<endl;
+//  cout<<y<<endl;
+// **********type casting******
+ int a =45;
+ float b=5.55;
+ cout << " this value of a is "<<(float)a;
+ cout << " this value of a is "<<float(a);
+ cout << " this value of a is "<<(int)a;
+ cout << " this value of a is "<<int(a);
+ int c =int (b);
+ cout << "this expression is "<< a+b;
+ cout << "this expression is "<< a+(int)b;
+ cout << "this expression is "<< a+int(b);
+ return 0;
+}
