7759f6cbec8884694fb12c4109c056d224a0ba98
kishor324 <kishorbhatt157@gmail.com>
2025-10-04 18:02:20 +0545

    Arrays in cpp

---
+++ video.13/tul13.cpp
@@ -0,0 +1,23 @@
+// code is incomplete i don't want to spend this topic
+#include <iostream>
+using namespace std;
+
+int main() {
+// Array examples.
+int marks []= { 23,33,22,34};
+int mathmarks [4];
+mathmarks[0]=278;
+
+mathmarks[1]=978;
+mathmarks[2]=228;
+mathmarks[4]=528;
+ cout<<" These are the mathematics marks "<<endl;
+// u can chnage value of array
+  for (int i = 0; i < 4; i++)
+  {
+    /* code */
+     cout<< " the value of marks"<<i<<" is"<<marks[1]<<endl;
+  }
+  
+return 0;
+}
